dc0b70a1258bf22059d7d7d41fe30beb8733560c
Asepabdaz <asepabdaz@gmail.com>
2019-09-18 15:18:58 +0700

    Initial Commit

---
+++ NanoChallange2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NanoChallange2
+//
+//  Created by Hai on 18/09/19.
+//  Copyright © 2019 Asep Abdaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
